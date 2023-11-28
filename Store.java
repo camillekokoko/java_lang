9b096cb0bd52be3b19fe96305b4eeeb630394450
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-11-29 02:18:09 +1030

    Update Store.java

--- Store.java
+++ Store.java
@@ -15,3 +15,24 @@ public class Store {
   public static void main(String[] args) {
     Store lemonadeStand = new Store("lemonade", 42, .99);
     Store cookieShop = new Store("cookies", 12, 3.75);
+
+  ---------------
+    public class Store {
+  
+  // new method: constructor!
+  public Store() {
+    System.out.println("I am inside the constructor method.");
+
+  }
+  
+  // main method is where we create instances!
+  public static void main(String[] args) {
+    System.out.println("Start of the main method.");
+    
+    // create the instance below
+    Store lemonadeStand = new Store();
+    // print the instance below
+    System.out.println(lemonadeStand);
+    
+  }
+}
