69a9bffba62e9a619c836dc6303d62e05757478e
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-11-29 01:20:35 +1030

    Create BakeSale.java

---
+++ BakeSale.java
@@ -0,0 +1,8 @@
+public class BakeSale {
+	public static void main(String[] args) {   
+		int numCookies = 17;
+    numCookies -= 3; // sell 3 cookies
+    numCookies /= 2; // sell half of the remaining cookies
+    System.out.println(numCookies);
+	}
+}
