406ab4c70337fd85064862bdaf6e51ed82348d9d
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-11-29 00:56:42 +1030

    Create MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Lin";
+    int age = 100;
+    double desiredSalary = 99999999.9;
+    char gender = 'f';
+    boolean lookingForJob = false;
+	}
+}
