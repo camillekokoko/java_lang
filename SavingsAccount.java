2e5deaf03c2d4365289a8d0fe1085c36a479956e
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-11-30 13:55:48 +1030

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -5,6 +5,58 @@ public class SavingsAccount {
   public SavingsAccount(int initialBalance){
     balance = initialBalance;
   }
+
+  
+  public void checkBalance(){
+    System.out.println("Hello!");
+    System.out.println("Your balance is "+ balance);
+  }
+
+  public void deposit(int amountToDeposit){
+    balance += amountToDeposit;
+    System.out.println("You just deposited "+ balance);
+  }
+
+  public int withdraw(int amountToWithdraw){
+    balance -= amountToWithdraw;
+    System.out.println("You just withdrew "+ amountToWithdraw);
+    return amountToWithdraw;
+  }
+
+  public static void main(String[] args){
+    SavingsAccount savings = new SavingsAccount(2000);
+  
+    //Check balance:
+    savings.checkBalance();
+    
+    //Withdrawing:
+    savings.withdraw(300);
+
+    //Check balance:
+    savings.checkBalance();
+    //Deposit:
+    savings.deposit(600);
+    //Check balance:
+    savings.checkBalance();
+    
+    //Deposit:
+    savings.deposit(600);
+    
+    //Check balance:
+    savings.checkBalance();    
+  }       
+}
+--------------------------------
+
+
+
+public class SavingsAccount {
+  
+  int balance;
+  
+  public SavingsAccount(int initialBalance){
+    balance = initialBalance;
+  }
   
   public static void main(String[] args){
     SavingsAccount savings = new SavingsAccount(2000);
