0928e70f8d79d1ac668188ae316c7b97f65144d7
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-11-30 15:09:39 +1030

    Update Order_switch.java

--- Order_switch.java
+++ Order_switch.java
@@ -48,3 +48,16 @@ public class Order {
     chemistrySet.ship();
   }
 }
+
+
+/*
+if-then:
+code block runs if condition is true
+if-then-else:
+one block runs if conditions is true
+another block runs if condition is false
+if-then-else chained:
+same as if-then but an arbitrary number of conditions
+switch:
+switch block runs if condition value matches case value
+*/
