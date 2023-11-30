e757fe53cedc387f222ecb188f6bb932588f25c1
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-11-30 20:50:22 +1030

    Update detectiveToDos.java

--- detectiveToDos.java
+++ detectiveToDos.java
@@ -72,3 +72,12 @@ class ToDos {
   
   
 }
+/*
+Creating an ArrayList.
+Adding a new ArrayList item using add().
+Accessing the size of an ArrayList using size().
+Finding an item by index using get().
+Changing the value of an ArrayList item using set().
+Removing an item with a specific value using remove().
+Retrieving the index of an item with a specific value using indexOf().
+*/
