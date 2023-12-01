1a28bd292aa9a862a2825b2c98ec1d29bbcd14a7
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-12-01 13:48:30 +1030

    Create Poetry.java

---
+++ Poetry.java
@@ -0,0 +1,21 @@
+/*
+length()
+concat()
+equals()
+indexOf()
+charAt()
+substring()
+toUpperCase() / toLowerCase()
+*/
+
+public class Poetry {
+  
+	public static void main(String[] args) {
+      
+    String line = "The Heav'ns and all the Constellations rung";     
+    // Change the arguments:
+    System.out.println(line.substring(4, 11));
+    
+  }
+  
+}
