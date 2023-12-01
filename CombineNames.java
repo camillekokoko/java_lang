07e390e555668bb73e725fd44bdb329460c8d2b3
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-12-01 13:41:51 +1030

    Create CombineNames.java

---
+++ CombineNames.java
@@ -0,0 +1,16 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    
+    // Add your first names:
+    String firstName = "Camille";  
+    String lastName = "Ko";
+
+    // What's the full name?
+    
+    System.out.println(firstName.concat(lastName));
+    
+    
+  }
+  
+}
