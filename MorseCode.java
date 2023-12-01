e83fcfff0f9517f6eacb07405051515636b7f6f4
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-12-01 13:44:46 +1030

    Create MorseCode.java

---
+++ MorseCode.java
@@ -0,0 +1,23 @@
+/*
+length()
+concat()
+equals()
+indexOf()
+charAt()
+substring()
+toUpperCase() / toLowerCase()
+*/
+
+
+public class MorseCode {
+  
+  public static void main(String[] args) {
+    
+    String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+    // Write the code:
+    System.out.println(code.indexOf(". .- -"));
+    
+  }
+  
+}
