7acc862b368f2f20c341c614bfa75ee1696944c6
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-12-01 13:47:22 +1030

    Create Initials.java

---
+++ Initials.java
@@ -0,0 +1,28 @@
+/*
+length()
+concat()
+equals()
+indexOf()
+charAt()
+substring()
+toUpperCase() / toLowerCase()
+*/
+
+
+public class Initials {
+  
+	public static void main(String[] args) {
+    
+    // Add a first name and a last name:
+    String firstName = "Camille";  
+    String lastName = "Ko";
+    
+    // What are the initials?
+    System.out.println(firstName.charAt(0));
+    System.out.println(lastName.charAt(0));
+
+
+    
+  }
+  
+}
