848089d3c8cf99f46788a913f278e73066b7958f
Camille Ko <48820497+camillekokoko@users.noreply.github.com>
2023-12-01 13:43:37 +1030

    Create Password.java

---
+++ Password.java
@@ -0,0 +1,22 @@
+/*
+length()
+concat()
+equals()
+indexOf()
+charAt()
+substring()
+toUpperCase() / toLowerCase()
+*/
+
+public class Password {
+  
+  public static void main(String[] args) {
+    
+    String password = "correcthorsebatterystaple";
+    
+    // Write the code:
+    System.out.println(password.equals("a"));
+    
+  }
+  
+}
